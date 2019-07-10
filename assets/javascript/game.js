ea2d415a5875554b5f1d0bf5f291b3315ef9b68a
Daryl Acut <darylacut@yahoo.com>
2019-07-10 06:29:53 -0400

    made basic html, values to some variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+// game displays a random number onscreen between 19-120
+
+//document.getElementById("#randomNumber").innerHTML = 
+
+function startgame () {
+
+    var randomNumber = Math.floor(Math.random() * (120 - 19 +1)) + 19;
+    var button1 = Math.floor(Math.random() * (12)) + 1;
+    var button2 = Math.floor(Math.random() * (12)) + 1;
+    var button3 = Math.floor(Math.random() * (12)) + 1;
+    var button4 = Math.floor(Math.random() * (12)) + 1;
+
+}
+
+startgame ();
+
+    $("button1").on("click", append())
+
+        if ( total === randomNumber );
+            wins++;
+        else if ( total > randomNumber );
+        losses++;
