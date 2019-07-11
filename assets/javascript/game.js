b777f77dd447401d5b14f99d109543bb38b4975b
Daryl Acut <darylacut@yahoo.com>
2019-07-11 05:31:44 -0400

    defined functions for clicking the button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,24 +3,40 @@
 //document.getElementById("#randomNumber").innerHTML = 
 
 var randomNumber = Math.floor(Math.random() * (120 - 19 +1)) + 19;
-    var button1 = Math.floor(Math.random() * (12)) + 1;
-    var button2 = Math.floor(Math.random() * (12)) + 1;
-    var button3 = Math.floor(Math.random() * (12)) + 1;
-    var button4 = Math.floor(Math.random() * (12)) + 1;
-    var wins
-    var losses
-    var total
-
+    var button1Value = Math.floor(Math.random() * (12)) + 1;
+    var button2Value = Math.floor(Math.random() * (12)) + 1;
+    var button3Value = Math.floor(Math.random() * (12)) + 1;
+    var button4Value = Math.floor(Math.random() * (12)) + 1;
+    var winsCount = 0
+    var lossesCount = 0
+    var totalValue = 0
 
 
 $(document).ready(function() {
 
-    $("button1").on("click," )
+
+    $("#button1").on("click," function() {
+        totalValue = button1Value + totalValue;
+        
+    }
     
-   
+    $("#button2").on("click," function() {
+        totalValue = button2Value + totalValue;
+    }
+
+    $("#button3").on("click," function() {
+        totalValue = button3Value + totalValue;
+    }
+    
+    $("#button4").on("click," function() {
+        totalValue = button4Value + totalValue;
+    }
+
+
+
     if ( total === randomNumber );
         wins++;
-        $('#winCount').text(winsCount);
+        $('#wins').text(winsCount);
 
 
     if ( total > randomNumber );
