e3c510b86f6ba1da8209d171a405e78d722046c5
Daryl Acut <darylacut@yahoo.com>
2019-07-14 23:10:03 -0400

    fixed functions, added check to win function.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,14 +2,45 @@
 
 //document.getElementById("#randomNumber").innerHTML = 
 
-var randomNumber = Math.floor(Math.random() * (120 - 19 +1)) + 19;
-    var button1Value = Math.floor(Math.random() * (12)) + 1;
-    var button2Value = Math.floor(Math.random() * (12)) + 1;
-    var button3Value = Math.floor(Math.random() * (12)) + 1;
-    var button4Value = Math.floor(Math.random() * (12)) + 1;
+
+
+    var randomNumber;
+    var button1Value;
+    var button2Value;
+    var button3Value;
+    var button4Value;
     var winsCount = 0
     var lossesCount = 0
-    var totalValue = 0
+    var totalValue;
+
+
+    function startGame() {
+        randomNumber = Math.floor(Math.random() * (120 - 19 +1)) + 19;
+        button1Value = Math.floor(Math.random() * (12)) + 1;
+        button2Value = Math.floor(Math.random() * (12)) + 1;
+        button3Value = Math.floor(Math.random() * (12)) + 1;
+        button4Value = Math.floor(Math.random() * (12)) + 1;
+        totalValue = 0
+    
+        $('#total').text(totalValue);
+        $('#wins').text(winsCount);
+        $('#losses').text(lossesCount);
+        $("#randomNumberBox").text(randomNumber);
+    }
+    
+    function checkIfWin() {
+        if ( totalValue == randomNumber ) {
+            winsCount++;
+            $('#wins').text(winsCount);
+            startGame();
+        }
+          
+         if ( totalValue > randomNumber ) {
+            lossesCount++;
+            $('#losses').text(lossesCount);
+            startGame();
+        }
+    }
 
 
  $(document).ready(function() {
@@ -22,34 +53,32 @@ $("#randomNumberBox").text(randomNumber);
     $("#button1").on("click", function() {
         totalValue = button1Value + totalValue;
         $("#total").text(totalValue);   
+        checkIfWin();
     });
     
     $("#button2").on("click", function() {
         totalValue = button2Value + totalValue;
         $("#total").text(totalValue);
+        checkIfWin();
     });
 
     $("#button3").on("click", function() {
         totalValue = button3Value + totalValue;
         $("#total").text(totalValue);
+        checkIfWin();
     });
     
     $("#button4").on("click", function() {
         totalValue = button4Value + totalValue;
         $("#total").text(totalValue);
+        checkIfWin();
     });
 
 
 
-    if ( totalValue === randomNumber ); {
-        winsCount++;
-        $('#wins').text(winsCount);
-    }
-      
-     if ( totalValue > randomNumber ); {
-        lossesCount++;
-        $('#losses').text(lossesCount);
-    }
+         startGame();
+
+    
        
 });
 
